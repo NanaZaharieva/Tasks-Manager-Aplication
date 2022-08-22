68e220c4d2641519dbea147181e4cb9602f384a4
Nana Zaharieva <nana.zaharieva@abv.bg>
2022-08-22 23:10:46 +0300

    Third commit

--- tasks/tasks.component.ts
+++ tasks/tasks.component.ts
@@ -22,6 +22,21 @@ done : ITask [] = [];
       item : ['', Validators.required]
     })
   }
+  addTask(){
+    this.tasks.push({
+      description:this.taskForm.value.item,
+      done:false
+    })
+  }
+  deleteTask(i: number){
+      this.tasks.splice(i,1)
+  }
+  deleteInProgressTask(i: number){
+    this.inprogress.splice(i,1)
+}
+deleDoneTask(i: number){
+  this.done.splice(i,1)
+}
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
